8bbd1b6a81673bfeabdffe688c7b1262f9eaa14c
Deyky Pinheiro <pinheiromikael96@gmail.com>
2023-10-31 11:25:45 -0300

    lexemes literals

--- src/main/java/org/example/Scanner.java
+++ src/main/java/org/example/Scanner.java
@@ -1,7 +1,9 @@
 package org.example;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import static org.example.TokenType.*;
 
@@ -15,6 +17,8 @@ public class Scanner {
 
     private int line = 1;
 
+    private static Map<String, TokenType> keywords = new HashMap<>();
+
 
     private final List<Token> tokens = new ArrayList<>();
 
@@ -24,7 +28,7 @@ public class Scanner {
 
     public List<Token> scanTokens() {
 
-        while (!isAEnd()) {
+        while (!isAtEnd()) {
             start = current;
             scanToken();
         }
@@ -33,7 +37,7 @@ public class Scanner {
     }
 
 
-    private boolean isAEnd() {
+    private boolean isAtEnd() {
         return current >= this.source.length();
     }
 
@@ -85,7 +89,7 @@ public class Scanner {
                 break;
             case '/':
                 if (match('/')) {
-                    while (peek() != '\n' && !isAEnd()) advance();
+                    while (peek() != '\n' && !isAtEnd()) advance();
                 } else {
                     addToken(SLASH);
                 }
@@ -97,14 +101,97 @@ public class Scanner {
             case '\n':
                 line++;
                 break;
+            case '"':
+                string();
+                break;
+
             default:
-                Lox.error(line, "Unexpected character.");
+                if (isDigit(c)) {
+                    number();
+                } else if (isAlpha(c)) {
+                    identifier();
+                } else {
+                    Lox.error(line, "Unexpected character.");
+                }
                 break;
         }
     }
 
+    private boolean isAlpha(char c) {
+        return (c >= 'a' && c <= 'z') ||
+                (c >= 'A' && c <= 'Z') ||
+                c == '_';
+    }
+
+    private boolean isAlphaNumeric(char c) {
+        return isAlpha(c) || isDigit(c);
+    }
+
+
+    private void identifier() {
+        while (isAlphaNumeric(peek())) advance();
+
+        String text = source.substring(start, current);
+        TokenType type = keywords.get(text);
+        if (type == null) {
+            type = IDENTIFIER;
+        }
+        addToken(type);
+    }
+
+    private void number() {
+        while (isDigit(peek())) {
+            advance();
+        }
+
+
+//        adianta caso tenha algum ponto no numero, proximo tem que se um digito pra continuar
+        if (peek() == '.' && isDigit(peekNext())) {
+            advance();
+        }
+
+        while (isDigit(peek())) {
+            advance();
+        }
+
+        addToken(NUMBER, Double.parseDouble(source.substring(start, current)));
+
+
+    }
+
+
+    private char peekNext() {
+        if (current + 1 >= source.length()) {
+            return '\n';
+        }
+        return source.charAt(current + 1);
+    }
+
+    private boolean isDigit(char c) {
+        return c >= '0' && c <= '9';
+    }
+
+    private void string() {
+        while (peek() != '"' && !isAtEnd()) {
+            if (peek() == '\n') {
+                line++;
+            }
+            advance();
+        }
+        if (isAtEnd()) {
+            Lox.error(line, "Unterminated string.");
+            return;
+        }
+
+//        para pular a  ultima -> "
+        advance();
+
+        String value = source.substring(start + 1, current + 1);
+        addToken(STRING, value);
+    }
+
     private boolean match(char expected) {
-        if (isAEnd()) return false;
+        if (isAtEnd()) return false;
         if (source.charAt(current) != expected) return false;
 
         current++;
@@ -125,8 +212,29 @@ public class Scanner {
     }
 
     private char peek() {
-        if (isAEnd()) return '\0';
+        if (isAtEnd()) return '\0';
         return source.charAt(current);
+    }
+
+
+    static {
+        keywords = new HashMap<>();
+        keywords.put("and", AND);
+        keywords.put("class", CLASS);
+        keywords.put("else", ELSE);
+        keywords.put("false", FALSE);
+        keywords.put("for", FOR);
+        keywords.put("fun", FUN);
+        keywords.put("if", IF);
+        keywords.put("nil", NIL);
+        keywords.put("or", OR);
+        keywords.put("print", PRINT);
+        keywords.put("return", RETURN);
+        keywords.put("super", SUPER);
+        keywords.put("this", THIS);
+        keywords.put("true", TRUE);
+        keywords.put("var", VAR);
+        keywords.put("while", WHILE);
     }
 
 }
